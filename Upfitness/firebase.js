985f187c62f8728feff700d00f519ea41b50437a
luisagnes <lixilipe@gmail.com>
2023-06-29 06:23:52 -0300

    Firebase #23

---
+++ Upfitness/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDoO4tkBhurLbQa3McU7HiXw3SXMIlpJ8o",
+  authDomain: "backend-up-fitness.firebaseapp.com",
+  projectId: "backend-up-fitness",
+  storageBucket: "backend-up-fitness.appspot.com",
+  messagingSenderId: "398147421854",
+  appId: "1:398147421854:web:790bd9543eeb1715ff9d8f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
